c54be6a6e6458811858ef5fee4f6c645cdb5ba8c
FrensiZ <zejnufre@outlook.com>
2025-01-18 17:38:44 +0000

    update

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import torch as th
+import pickle
 
 def create_sequences(data, seq_length, stride, change_type='pct'):
     # Calculate changes
@@ -52,3 +52,55 @@ def to_onehot_inference(tokens, vocab_size):
     """Efficient one-hot encoding using scatter"""
     onehot = th.zeros(1, 1, vocab_size)
     return onehot.scatter_(2, tokens.view(1, 1, 1), 1)
+
+power_data = pd.read_csv('processed_power_data.csv')
+delta_data = create_sequences(power_data['Global_active_power'].values, seq_length=52, stride=20, change_type='delta')
+token_limit = 2
+
+bin_width = np.std(np.concatenate(delta_data)) / 32
+bin_start=-token_limit
+bin_stop=token_limit
+token_size = int((bin_stop - bin_start) / bin_width)  # Calculate number of bins
+
+token_data = tokenize_deltas(delta_data, bin_start=bin_start, bin_stop=bin_stop, bin_width=bin_width)
+
+n_sequences = delta_data.shape[0]
+n_days = delta_data.shape[1] 
+
+train_ratio = 0.75
+# Chronological splitting
+train_size = int(train_ratio * n_sequences)
+val_size = int((1-train_ratio)/2 * n_sequences)
+
+# Split maintaining temporal order
+train_set = delta_data[:train_size]
+val_set = delta_data[train_size:train_size+val_size]
+test_set = delta_data[train_size+val_size:]
+
+X_train = tokenize_deltas(train_set, bin_start=bin_start, bin_stop=bin_stop, bin_width=bin_width)
+X_val = tokenize_deltas(val_set, bin_start=bin_start, bin_stop=bin_stop, bin_width=bin_width)
+X_test = tokenize_deltas(test_set, bin_start=bin_start, bin_stop=bin_stop, bin_width=bin_width)
+
+print("Training set shape:", X_train.shape)
+print("Validation set shape:", X_val.shape)
+print("Test set shape:", X_test.shape)
+
+# Save everything needed for training
+data_dict = {
+    'X_train': X_train,
+    'X_val': X_val,
+    'X_test': X_test,
+    'token_size': token_size,
+    'token_limit': token_limit,
+    'bin_start': bin_start,
+    'bin_stop': bin_stop,
+    'bin_width': bin_width
+}
+
+with open('processed_data.pkl', 'wb') as f:
+    pickle.dump(data_dict, f)
+
+print("Data saved with shapes:")
+print("Training set:", X_train.shape)
+print("Validation set:", X_val.shape)
+print("Test set:", X_test.shape)
--- train.py
+++ train.py
@@ -1,22 +1,56 @@
 # Same imports as data_prep.py
-import numpy as np
-import torch as th
-import torch.nn as nn
-import torch.optim as optim
-import pandas as pd
-import os
-from collections import Counter
-import matplotlib.pyplot as plt
-from statsmodels.tsa.stattools import acf
-from scipy.stats import wasserstein_distance, gaussian_kde
-import seaborn as sns
-from scipy.stats import skew, kurtosis
-import gymnasium as gym
-from gymnasium import spaces
-from sb3_contrib import RecurrentPPO
-from stable_baselines3.common.monitor import Monitor
+# import numpy as np
+# import torch as th
+# import torch.nn as nn
+# import torch.optim as optim
+# import pandas as pd
+# import os
+# from collections import Counter
+# import matplotlib.pyplot as plt
+# from statsmodels.tsa.stattools import acf
+# from scipy.stats import wasserstein_distance, gaussian_kde
+# import seaborn as sns
+# from scipy.stats import skew, kurtosis
+# import gymnasium as gym
+# from gymnasium import spaces
+# from sb3_contrib import RecurrentPPO
+# from stable_baselines3.common.monitor import Monitor
+
+from data_processing import *
+from models import *
+from visualization import *
 
 
-from data_processing import create_sequences, tokenize_deltas
-from models import LSTMModel, LSTM_Discriminator, CustomEnv
-from visualization import plot_price_token, supervised_wasserstein
+
+power_data = pd.read_csv('processed_power_data.csv')
+delta_data = create_sequences(power_data['Global_active_power'].values, seq_length=52, stride=20, change_type='delta')
+token_limit = 2
+
+bin_width = np.std(np.concatenate(delta_data)) / 32
+bin_start=-token_limit
+bin_stop=token_limit
+token_size = int((bin_stop - bin_start) / bin_width)  # Calculate number of bins
+
+token_data = tokenize_deltas(delta_data, bin_start=bin_start, bin_stop=bin_stop, bin_width=bin_width)
+
+
+n_sequences = delta_data.shape[0]
+n_days = delta_data.shape[1] 
+
+train_ratio = 0.75
+# Chronological splitting
+train_size = int(train_ratio * n_sequences)
+val_size = int((1-train_ratio)/2 * n_sequences)
+
+# Split maintaining temporal order
+train_set = delta_data[:train_size]
+val_set = delta_data[train_size:train_size+val_size]
+test_set = delta_data[train_size+val_size:]
+
+X_train = tokenize_deltas(train_set, bin_start=bin_start, bin_stop=bin_stop, bin_width=bin_width)
+X_val = tokenize_deltas(val_set, bin_start=bin_start, bin_stop=bin_stop, bin_width=bin_width)
+X_test = tokenize_deltas(test_set, bin_start=bin_start, bin_stop=bin_stop, bin_width=bin_width)
+
+print("Training set shape:", X_train.shape)
+print("Validation set shape:", X_val.shape)
+print("Test set shape:", X_test.shape)
